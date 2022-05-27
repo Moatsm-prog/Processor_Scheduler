43c013cea847a081496f227faf502179291a4cea
Muhammad Ahmad Thabet <53876326+ZeusDes@users.noreply.github.com>
2022-05-27 21:28:49 +0200

    checking for entry start & end time

--- Sources/TimeLine.cpp
+++ Sources/TimeLine.cpp
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 bool TimeLine::addProcess(Process p, float start_time, float end_time){
+    if(end_time <= start_time) return 0;
     if(entries.size()){
         TimeEntry lastE = entries.back();
-        if(start_time < lastE.getEnd_time() || end_time <= start_time) return 0; 
+        if(start_time < lastE.getEnd_time()) return 0; 
     }
+
     entries.push_back(TimeEntry(p, start_time, end_time));
     return 1;
 }
